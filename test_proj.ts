963b2e1484522f07912270bdb43c743108cdbc8b
Wirl <dinhlongrmx@gmail.com>
2026-06-24 08:01:34 +0700

    Fix ts import

--- test_proj.ts
+++ test_proj.ts
@@ -1,4 +1,4 @@
-import { getProjectBySlug } from "./src/lib/projects.ts";
+import { getProjectBySlug } from "./src/lib/projects";
 
 const p = getProjectBySlug("thi-cong-xay-lap-hang-muc-chieu-sang-1");
 console.log(JSON.stringify(p, null, 2));
